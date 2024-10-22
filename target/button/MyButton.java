8b2e10c0faa9bb18ed0ed0f8b28ce82e6eccd482
nguyenquanghuyyy <huyccht123@gmail.com>
2024-10-22 21:41:45 +0700

    sua target

---
+++ target/button/MyButton.java
@@ -0,0 +1,95 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package button;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import javax.swing.JButton;
+
+/**
+ *
+ * @author PC
+ */
+public class MyButton extends JButton {
+    public MyButton() {
+        //init color
+        setColor(Color.WHITE);
+        colorOver = new Color(179, 250, 160);
+        colorClick = new Color(152, 182, 144);
+        borderColor = new Color(30, 136, 56);
+        setContentAreaFilled(false);
+    }
+    
+    private boolean over;
+    private Color color;
+    private Color colorOver;
+    private Color colorClick;
+    private Color borderColor;
+    private int radius = 0;
+
+    public boolean isOver() {
+        return over;
+    }
+
+    public void setOver(boolean over) {
+        this.over = over;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+        setBackground(color);
+    }
+
+    public Color getColorOver() {
+        return colorOver;
+    }
+
+    public void setColorOver(Color colorOver) {
+        this.colorOver = colorOver;
+    }
+
+    public Color getColorClick() {
+        return colorClick;
+    }
+
+    public void setColorClick(Color colorClick) {
+        this.colorClick = colorClick;
+    }
+
+    public Color getBorderColor() {
+        return borderColor;
+    }
+
+    public void setBorderColor(Color borderColor) {
+        this.borderColor = borderColor;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+    
+    @Override
+    protected void paintComponent(Graphics graphics) {
+        Graphics2D g2 = (Graphics2D)graphics;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        //paint border
+        g2.setColor(borderColor);
+        g2.fillRoundRect(0, 0, getWidth(), getHeight(), radius, radius);
+        g2.setColor(getBackground());
+        //border set 2 pix
+        g2.fillRoundRect(2, 2, getWidth() - 4, getHeight() - 4, radius, radius);
+        super.paintComponent(graphics);
+    }
+}
